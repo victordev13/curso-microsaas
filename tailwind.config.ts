0f9b181a388f03a90b77dd2fd384b77155cb3865
victordev13 <vtrcarvalho.13@gmail.com>
2025-01-21 20:59:17 -0300

    feat: theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        "background—primary": "#050505",
+        "background—secondary": "#0F0F10",
+        "background—tertiary": "#19191A",
+        "content-body": "#CDCBCC",
+        "content—ptaceholder": "#827D7F",
+        "content-headline": "#B2B2B2",
+        "border—primary": "#19191A",
+        "border—secondary": "#323234",
+        "border—tertiary": "#979798",
+        "accent-purple": "#4B2DBB",
+        "accent—green": "#87BB2D",
+        "accent-pink": "#B5446B"
+      }
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 } satisfies Config;
