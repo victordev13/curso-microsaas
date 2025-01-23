c983fdc1c06a24fce2d924030015a658e6179dba
victordev13 <vtrcarvalho.13@gmail.com>
2025-01-23 20:52:09 -0300

    chore: eslint config, change to pnpm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  {
+    rules: {
+      camelcase: 'off',
+    },
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    '@rocketseat/eslint-config/next',
+  ),
+]
 
-export default eslintConfig;
+export default eslintConfig
