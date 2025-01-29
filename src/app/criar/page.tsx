b04dc4d06ed5d7ef14a46b5a045b1e1f6c52899e
victordev13 <vtrcarvalho.13@gmail.com>
2025-01-28 21:10:39 -0300

    feat: create link page

---
+++ src/app/criar/page.tsx
@@ -0,0 +1,27 @@
+import { Rocket } from 'lucide-react'
+import { Header } from '../components/landing-page/header'
+import { Button } from '../components/ui/button'
+import { Input } from '../components/ui/input'
+
+export default function Criar() {
+  return (
+    <div>
+      <Header />
+      <div className="h-screen flex flex-col gap-10 items-center justify-center max-w-xl mx-auto">
+        <div className="flex items-center gap-4">
+          <h1 className="text-4xl font-bold text-white">Escolha seu link</h1>
+          <Rocket className="size-10" />
+        </div>
+
+        <form className="w-full flex items-center gap-2">
+          <span className="text-white">eudev.test/</span>
+          <Input />
+          <Button className="w-[126px]">Criar</Button>
+        </form>
+        <div>
+          <span className="text-accent-pink">Erro aleatório</span>
+        </div>
+      </div>
+    </div>
+  )
+}
